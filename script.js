0b2904c71067bf47b90b4512c84f87eb25f6b66d
aditya0801-shibhu <adityaprakashsingh993440@gmail.com>
2025-09-20 18:55:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function showMessage() {
+  document.getElementById("msg").style.display = "block";
+}
+
+// create floating hearts
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.innerHTML = "❤";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.fontSize = Math.random() * 20 + 15 + "px";
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 6000);
+}
+
+setInterval(createHeart, 500);
